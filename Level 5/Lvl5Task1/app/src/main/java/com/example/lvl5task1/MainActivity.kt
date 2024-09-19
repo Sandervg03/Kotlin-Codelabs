78e3b9e7322e2ee4eeec15a5062c5606943b5e84
Sander van Goch <sander.van.goch@hva.nl>
2024-09-19 17:31:08 +0200

    Adds Gradle

---
+++ Level 5/Lvl5Task1/app/src/main/java/com/example/lvl5task1/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.lvl5task1
+
+import android.app.Application
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.lvl5task1.ui.screens.Screens
+import com.example.lvl5task1.ui.screens.addscreen.AddScreen
+import com.example.lvl5task1.ui.screens.homescreen.HomeScreen
+import com.example.lvl5task1.ui.theme.Lvl5Task1Theme
+import com.example.lvl5task1.viewmodel.GameViewModel
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lvl5Task1Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    AppNavHost(
+                        navController = rememberNavController(),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+    
+    @Composable
+    private fun AppNavHost(
+        navController: NavHostController,
+        modifier: Modifier
+        ) {
+
+        val viewModel: GameViewModel = viewModel()
+
+        Column(
+            modifier = modifier
+        ) {
+            NavHost(navController = navController, startDestination = Screens.HomeScreen.route) {
+
+                composable(route = Screens.HomeScreen.route) {
+                    HomeScreen().Create(navController = navController, viewModel = viewModel)
+                }
+
+                composable(route = Screens.AddScreen.route) {
+                    AddScreen().Create(navController = navController, viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
